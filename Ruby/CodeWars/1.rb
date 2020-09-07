1fe781ed2c2b1c159097de658c9d773a7bf28684
Oscar M Olivera <oscarmolivera@gmail.com>
2020-09-07 11:11:33 -0300

    Modified Files

--- Ruby/CodeWars/1.rb
+++ Ruby/CodeWars/1.rb
@@ -2,18 +2,35 @@
 
 =end
 
-def test(data)
-  
+def test(str)
+  arr = []
+  str.chars.map{|chr| arr.unshift(chr)}
+  arr.join('')
 end
 
-p test(data)
+p test('Hello world and coders')
 # => 'test ouput'
 
 =begin ############################################# OTHERS SOLUTIONS 
 2)
-
+def test(str)
+  reverse_phrase = []
+  str.split(' ').each do |word|
+    reverse_word = []
+    word.chars.each do |chr|
+      reverse_word.unshift(chr)
+    end
+    reverse_phrase.unshift(reverse_word.join(''))
+  end
+  reverse_phrase.join(' ')
+end
 ----------------------------------------------------------------------
 3)
+def test(str)
+  arr = []
+  str.chars.map{|chr| arr.unshift(chr)}
+  arr.join('')
+end
 
 ----------------------------------------------------------------------
 4)
