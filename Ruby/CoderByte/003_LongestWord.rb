f2ab7c4340d7704cda541fe0d754a37ba13f3208
Oscar M Olivera <oscarmolivera@gmail.com>
2020-12-27 15:53:46 -0300

    New CoderByte 003

--- Ruby/CoderByte/003_LongestWord.rb
+++ Ruby/CoderByte/003_LongestWord.rb
@@ -1,6 +1,9 @@
 =begin
 Challenge
-Have the function LongestWord(sen) take the sen parameter being passed and return the largest word in the string. If there are two or more words that are the same length, return the first word from the string with that length. Ignore punctuation and assume sen will not be empty.
+Have the function LongestWord(sen) take the sen parameter being passed and return 
+the largest word in the string. If there are two or more words that are the same 
+length, return the first word from the string with that length. Ignore punctuation
+and assume sen will not be empty.
 Sample Test Cases
 -----------------------
 Input:"fun&!! time"
@@ -11,11 +14,10 @@ Output:love
 =end
 
 def LongestWord(sen)
-
-  # code goes here
-  arr = sen.split(' ').map{|r| r.match(/[\w]+/).to_s}.max { |a, b| a.length <=> b.length }
+  sen.split(' ').map{|r| r.match(/[\w]+/).to_s}.max { |a, b| a.length <=> b.length }
 end
-   
+
+
 # keep this function call here    
 puts puts LongestWord("Argument goes here")  
 puts puts LongestWord("I love dogs")  
@@ -25,14 +27,14 @@ puts puts LongestWord("fun&!! time ")
 ### OTHERS SOLUTIONS
 def LongestWord(sen)
   longest = ""
-  sen.scan(/w+/).each{|word| longest = word if word.length > longest.length}
+  sen.scan(/\w+/).each{|word| longest = word if word.length > longest.length}
   longest
 end
 
 def LongestWord(sen)
   sen.scan(/\w+/).max_by(&:size)
- end
+end
 
- def LongestWord(sen)
-  sen.gsub(/[^a-zA-Z0-9" "]/,"").split(" ").sort_by(&:length).last
+def LongestWord(sen)
+  sen.gsub(/[^a-zA-Z0-9\s]/,"").split(" ").sort_by(&:length).last
 end
