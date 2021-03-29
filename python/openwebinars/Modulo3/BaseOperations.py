8d26ef1cb84de4ce96f3ec27b2796630ea5d3d0c
Oscar Olivera <oscarmolivera@gmail.com>
2021-03-29 14:21:01 -0300

    OW - Base Operaciontes

---
+++ python/openwebinars/Modulo3/BaseOperations.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+
+print("***** Operaciones de Suma Resta Multiplicacion y Division! *****")
+num1 = input("Ingrese el primer numero:")
+num2 = input("Ingrese el segundo numero:")
+sma = float(num1) + float(num2)
+res = float(num1) - float(num2)
+mul = float(num1) * float(num2)
+div = float(num1) / float(num2)
+print("Suma:%.2f, Resta:%.2f, Multiplicación:%.2f, División:%.2f"%(sma, res, mul, div))
