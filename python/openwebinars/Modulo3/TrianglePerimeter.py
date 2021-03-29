56560a7b4581ad2bc064b2d4891885dd718ea5ac
Oscar Olivera <oscarmolivera@gmail.com>
2021-03-29 14:22:33 -0300

    OW - TriaglePerimeter

---
+++ python/openwebinars/Modulo3/TrianglePerimeter.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+
+
+# Exceute by: python3 SimpleProgram.py 
+print("***** Calculemos el Perimetro de un triángulo! *****")
+base = input("Ingrese en metros la base del triángulo:")
+altura = input("Ingrese ahora tambien en metros la altura de dicho triángulo:")
+perimetro = (int(base) * int(altura)) / 2
+print("El perimetro de dicho triángulo es de  %s" % str(perimetro))
