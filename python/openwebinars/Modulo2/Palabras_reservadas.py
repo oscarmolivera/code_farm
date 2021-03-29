ef2ef8e3b45ea15c0f3bc9611eeed5965fc1e0db
Oscar Olivera <oscarmolivera@gmail.com>
2021-03-29 14:20:43 -0300

    OW - Palabras Reservas

---
+++ python/openwebinars/Modulo2/Palabras_reservadas.py
@@ -0,0 +1,33 @@
+# Un programa python está formado por instrucciones que acaban en un caracter 
+# de “salto de línea”.
+
+# El punto y coma “;” se puede usar para separar varias sentencias en una 
+# misma línea, pero no se aconseja su uso.
+
+# Una línea empiza en la primera posición, si tenemos instrucciones dentro de 
+# un bloque de una estrucura de control de flujo habra que hacer una 
+# identación.
+
+# La identación se puede hacer con espacios y tabulaciones pero ambos tipos no 
+# se pueden mezclar. Se recomienda usar 4 espacios.
+
+# La barra invertida “\“ al final de línea se emplea para dividir una línea 
+# muy larga en dos o más líneas.
+
+# Las expresiones entre paréntesis “()”, llaves “{}” y corchetes “[]” 
+# separadas por comas “, ” se pueden escribir ocupando varias líneas.
+
+# Cuando el bloque a sangrar sólo ocupa una línea ésta puede escribirse 
+# después de los dos punto.
+
+
+def reserved_words_in_python
+    [
+        "False", "class", "finally", "is", "return
+        "None", "continue", "for", "lambda", "try
+        "True", "def", "from", "nonlocal", "while
+        "and", "del", "global", "not", "with
+        "as", "elif", "if", "or", "yield
+        "assert", "else", "import", "pass
+        "break", "except", "in", "raise
+    ]
