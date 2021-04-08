6e1fe2b3514dad7bc26bb9eab10f6295eb864f24
Oscar Olivera <oscarmolivera@gmail.com>
2021-04-07 21:59:34 -0400

    RUBY: HackerRank Control Structures

---
+++ Ruby/HakerRank/TutorialRuby/B_Control Structures/a_each.rb
@@ -0,0 +1,19 @@
+=begin
+
+
+=end
+
+def method(**params)
+  params
+end
+
+p method(2, 3)
+# => response
+p method(2, 3)
+# => response
+p method(2, 3)
+# => response
+p method(2, 3)
+# => response
+p method(2, 3)
+# => response
