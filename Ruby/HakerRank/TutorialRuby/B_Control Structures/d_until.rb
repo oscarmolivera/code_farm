352a858eaa457375721b36056072ced0278afa65
Oscar Olivera <oscarmolivera@gmail.com>
2021-04-11 16:39:32 -0400

    HACKERRANK: Tutorial Until

--- Ruby/HakerRank/TutorialRuby/B_Control Structures/d_until.rb
+++ Ruby/HakerRank/TutorialRuby/B_Control Structures/d_until.rb
@@ -1,10 +1,41 @@
 =begin
 
+This challenge has a beautiful one-liner answer. "A hacker practices on
+HackerRank until getting to a rating of O(1) read as (Oh-one)"
+Call the method coder.practice until coder.oh_one? becomes true.
+Use the until control structure. until is the logical equivalent of while not.
+
+Hint
+```
+  while not <condition>
+    <code>
+  end
+
+or
+```
+  until <condition>
+    <code>
+  end
+
+or the beautiful one-liner
+```
+  <code> until <condition>
 
 =end
 
 def method(**params)
-  params
+  # Solution 1
+  while not coder.oh_one?
+    coder.practice
+  end
+
+  # Solution 2
+  until coder.oh_one?
+    coder.practice
+  end
+
+  #Solution 3
+  coder.practice until coder.oh_one?
 end
 
 p method(2, 3)
