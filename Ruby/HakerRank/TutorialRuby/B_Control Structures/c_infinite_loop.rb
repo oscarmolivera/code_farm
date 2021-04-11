9771eda73eb498242d28583bd28eb8a0da786a6f
Oscar Olivera <oscarmolivera@gmail.com>
2021-04-11 15:33:51 -0400

    HackerRank: Infinity Loop

--- Ruby/HakerRank/TutorialRuby/B_Control Structures/c_infinite_loop.rb
+++ Ruby/HakerRank/TutorialRuby/B_Control Structures/c_infinite_loop.rb
@@ -1,10 +1,33 @@
 =begin
 
+This is a wonderful exercise which explains the concept of infinite loops.
+
+"A hacker practices on HackerRank until getting to a rating of O(1) read as
+(Oh-one)"
+
+An infinite loop in Ruby is of the form
+
+```
+  loop do
+  end
+
+Use an infinite loop and call the method coder.practice within it and break if
+coder.oh_one? is true. "break" if conditions in Ruby are of the form
+
+```
+  if <condition>
+    break
+  end
+or
+  break if <condition>
 
 =end
-
-def method(**params)
-  params
+# Tutorial Solution
+def method(*params)
+  loop do
+    coder.practice
+    break if coder.oh_one?
+  end
 end
 
 p method(2, 3)
