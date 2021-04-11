c7234ae0aa995a99e3a54c9526d96ecb5bbdd5c7
Oscar Olivera <oscarmolivera@gmail.com>
2021-04-11 14:44:13 -0400

    HACKERRANK: Unless Example

---
+++ Ruby/HakerRank/TutorialRuby/B_Control Structures/b_unless.rb
@@ -0,0 +1,35 @@
+=begin
+
+You've updated the score of every HackerRank user who participated in a contest.
+Sometimes, HackerRank admins also participate in a given contest but care is
+taken to ensure that their submissions do not get any score and their score is
+not updated.
+
+Like the previous challenge, you are given a method scoring with an array passed
+as an argument. Each element of the array is of class User.
+
+User has two public methods, is_admin? and update_score. Your task in this
+challenge is to use the control structure unless and update all elements of the
+array who are not admins
+
+Hint
+```
+  unless user.is_admin?
+    user.update_score
+  end
+  # other
+  user.update_score unless user.is_admin?
+
+The above code is a Ruby one liner.
+
+Explanation
+unless is the logical equivalent of if not
+
+=end
+
+def scoring(array)
+  array.each{ |user| user.update_score unless user.is_admin? }
+end
+
+p scoring(array)
+# => response
